d6b141cb6205bf0b5dfbf60ee2bb7c561c866821
Chris Grandin <chris.grandin@gmail.com>
2021-12-05 03:21:59 -0800

    Add ability to have model executable either on the PATH or in a local directory * Fix syntax issues in .sample_admb() to make chanegs easier

--- R/sample_admb.R
+++ R/sample_admb.R
@@ -1,5 +1,3 @@
-
-
 #' @rdname wrappers
 #' @export
 sample_nuts <- function(model, path=getwd(), iter=2000, init=NULL, chains=3, warmup=NULL,
@@ -56,8 +54,6 @@ sample_rwm <- function(model, path=getwd(), iter=2000, init=NULL, chains=3, warm
                skip_unbounded=skip_unbounded,
                admb_args=admb_args)
 }
-
-
 
 #' Bayesian inference of an ADMB model using the no-U-turn
 #' sampler (NUTS) or random walk Metropolis (RWM) algorithms.
@@ -224,177 +220,206 @@ sample_admb <- function(model, path=getwd(), iter=2000, init=NULL, chains=3, war
                admb_args=admb_args)
 }
 
-
 #' Hidden wrapper function for sampling from ADMB models
 #'
 #' @inheritParams wrappers
 #' @param algorithm The algorithm to use, one of "NUTS" or "RWM"
-#'
-.sample_admb <- function(model, path=getwd(), iter=2000, init=NULL, chains=3, warmup=NULL,
-                        seeds=NULL, thin=1, mceval=FALSE, duration=NULL,
-                        parallel=FALSE, cores=NULL, control=NULL,
-                        algorithm="NUTS", skip_optimization=TRUE,
-                        skip_monitor=FALSE, skip_unbounded=TRUE,
-                        admb_args=NULL){
-  if(is.null(cores)) cores <- parallel::detectCores()-1
+.sample_admb <- function(model,
+                         path = getwd(),
+                         iter = 2000,
+                         init = NULL,
+                         chains = 3,
+                         warmup = NULL,
+                         seeds = NULL,
+                         thin = 1,
+                         mceval = FALSE,
+                         duration = NULL,
+                         parallel = FALSE,
+                         cores = NULL,
+                         control = NULL,
+                         algorithm = "NUTS",
+                         skip_optimization = TRUE,
+                         skip_monitor = FALSE,
+                         skip_unbounded = TRUE,
+                         admb_args = NULL){
+
   cores.max  <- parallel::detectCores()
+  if(is.null(cores)){
+    cores <- cores.max - 1
+  }
   if(cores > cores.max) {
-    cores <- cores.max-1
-    warning(paste('Specified cores larger than available, using total-1=', cores))
+    cores <- cores.max - 1
+    warning(paste0("Specified cores larger than available, using total - 1 = ", cores))
   }
   stopifnot(is.numeric(cores))
-  if(!is.null(control) & !is.list(control))
-    stop("control argument invalid, must be a list")
-  if(cores<1) stop(paste("Cores must be >=1, but is", cores))
-  parallel <- ifelse(cores==1 | chains ==1, FALSE, TRUE)
-  if(parallel & cores < chains)
-    message(paste("Recommend using chains < cores=", cores))
-  stopifnot(thin >=1); stopifnot(chains >= 1)
-  if(is.null(seeds)) seeds <- sample.int(1e7, size=chains)
-  if(iter < 1 | !is.numeric(iter)) stop("iter must be > 1")
-  ## Catch path and model name errors early
-  stopifnot(is.character(path)); stopifnot(is.character(model))
-  if(!dir.exists(path)) stop(paste('Folder', path, 'does not exist. Check argument \'path\''))
-  if (.Platform$OS.type=="windows") {
-    ff <- file.path(path, paste(model,".exe",sep=""))
-  } else {
-    ff <- file.path(path, paste("./",model,sep=""))
+  if(!is.null(control) && !is.list(control)){
+    stop("Control argument invalid, must be a list",
+         call. = FALSE)
   }
-  if(!file.exists(ff)) stop(paste('File', ff, 'not found. Check \'path\' and \'model\' arguments'))
-  v <- .check_ADMB_version(model=model, path=path, warn= algorithm=='NUTS')
-  if(v<=12.0 & !skip_unbounded) {
-    warning(paste('Version', v, 'of ADMB is incompatible with skip_unbounded=FALSE, ignoring'))
+  if(cores < 1){
+    stop(paste0("Cores must be >= 1, but is ", cores),
+         call. = FALSE)
+  }
+  parallel <- (cores == 1 || chains == 1)
+  if(parallel && cores < chains){
+    message(paste0("Recommend using chains < cores = ", cores))
+  }
+  stopifnot(thin >= 1)
+  stopifnot(chains >= 1)
+  if(is.null(seeds)){
+    seeds <- sample.int(1e7, size = chains)
+  }
+  if(iter < 1 || !is.numeric(iter)){
+    stop("iter must be > 1",
+         call. = FALSE)
+  }
+
+  model <- get_model_executable(model, path)
+
+  v <- .check_ADMB_version(model = model,
+                           path = path,
+                           warn = (algorithm == "NUTS"))
+  if(v <= 12.0 && !skip_unbounded){
+    warning(paste0("Version ", v, " of ADMB is incompatible with skip_unbounded = FALSE, ignoring"))
     skip_unbounded <- TRUE
   }
-  ## Update control with defaults
-  if(is.null(warmup)) warmup <- floor(iter/2)
-  if(!(algorithm %in% c('NUTS', 'RWM')))
-    stop("Invalid algorithm specified")
-  if(algorithm=='NUTS')
+  # Update control with defaults
+  if(is.null(warmup)){
+    warmup <- floor(iter / 2)
+  }
+  if(!(algorithm %in% c("NUTS", "RWM"))){
+    stop("Invalid algorithm specified",
+         call. = FALSE)
+  }
+  if(algorithm == "NUTS")
     control <- .update_control(control)
   if(is.null(init)){
-    warning('Using MLE inits for each chain -- strongly recommended to use dispersed inits')
-  }  else if(is.function(init)){
+    warning("Using MLE inits for each chain -- strongly recommended to use dispersed inits")
+  }else if(is.function(init)){
     init <- lapply(1:chains, function(x) init())
-  } else if(!is.list(init)){
-    stop("init must be NULL, a list, or a function")
+  }else if(!is.list(init)){
+    stop("init must be NULL, a list, or a function",
+         call. = FALSE)
   }
-  if(!is.null(init) & length(init) != chains){
-    stop("Length of init does not equal number of chains.")
+  if(!is.null(init) && length(init) != chains){
+    stop("Length of init does not equal number of chains",
+         call. = FALSE)
   }
-  ## Delete any psv files in case something goes wrong we dont use old
-  ## values by accident
-  trash <- suppressWarnings(file.remove(list.files(path)[grep('.psv', x=list.files())]))
-  trash <- suppressWarnings(file.remove(file.path(path, 'adaptation.csv'),
-                       file.path(path, 'unbounded.csv')))
-  ## Run in serial
-  if(!parallel){
-    if(algorithm=="NUTS"){
-      mcmc.out <- lapply(1:chains, function(i)
-        sample_admb_nuts(path=path, model=model, warmup=warmup, duration=duration,
-                         iter=iter, init=init[[i]], chain=i,
-                         seed=seeds[i], thin=thin,
-                         control=control, admb_args=admb_args,
-                         skip_optimization=skip_optimization))
-    } else {
-      mcmc.out <- lapply(1:chains, function(i)
-        sample_admb_rwm(path=path, model=model, warmup=warmup, duration=duration,
-                        iter=iter, init=init[[i]], chain=i,
-                        seed=seeds[i], thin=thin,
-                        control=control,
-                        skip_optimization=skip_optimization,
-                        admb_args=admb_args))
-    }
-    ## Parallel execution
-  } else {
+  # Delete any psv files, adaptation.csv, and unbounded.csv in case something goes wrong we don't use old
+  # values by accident
+  files_to_trash <- grep("\\.psv|adaptation\\.csv|unbounded\\.csv", dir(path), value = TRUE)
+  if(length(files_to_trash)){
+    unlink(file.path(path, files_to_trash))
+  }
+  if(parallel){
     snowfall::sfStop()
-    snowfall::sfInit(parallel=TRUE, cpus=cores)
-    ## errors out with empty workspace
-    if(length(ls(envir=globalenv()))>0)
+    snowfall::sfInit(parallel = TRUE, cpus = cores)
+    # Errors out with empty workspace
+    if(length(ls(envir = globalenv()))){
       snowfall::sfExportAll()
+    }
     on.exit(snowfall::sfStop())
     mcmc.out <- snowfall::sfLapply(1:chains, function(i)
-      sample_admb_parallel(parallel_number=i, path=path, model=model,
-                           duration=duration,
-                           algorithm=algorithm,
-                           iter=iter, init=init[[i]], warmup=warmup,
-                           seed=seeds[i], thin=thin,
-                           control=control,
-                           skip_optimization=skip_optimization,
-                           admb_args=admb_args))
+      sample_admb_parallel(parallel_number = i, path = path, model = model, duration=duration,
+                           algorithm = algorithm, iter = iter, init = init[[i]], warmup = warmup,
+                           seed = seeds[i], thin = thin, control = control,
+                           skip_optimization = skip_optimization, admb_args = admb_args))
+  }else{
+    mcmc.out <- lapply(1:chains, function(i)
+      sample_admb_nuts(path = path, model = model, warmup = warmup, duration = duration,
+                       iter = iter, init = init[[i]], chain = i, seed = seeds[i],
+                       thin = thin, control = control, admb_args = admb_args,
+                       skip_optimization = skip_optimization))
   }
-    warmup <- mcmc.out[[1]]$warmup
-    mle <- .read_mle_fit(model=model, path=path)
-    if(is.null(mle)){
-      par.names <- dimnames(mcmc.out[[1]]$samples)[[2]]
-      par.names <- par.names[-length(par.names)]
-    } else {
-      par.names <- mle$par.names
-    }
-    iters <- unlist(lapply(mcmc.out, function(x) dim(x$samples)[1]))
-    if(any(iters!=iter/thin)){
+
+  warmup <- mcmc.out[[1]]$warmup
+  mle <- .read_mle_fit(model = model, path = path)
+  browser()
+  if(is.null(mle)){
+    par.names <- dimnames(mcmc.out[[1]]$samples)[[2]]
+    par.names <- par.names[-length(par.names)]
+  }else{
+    par.names <- mle$par.names
+  }
+  iters <- unlist(lapply(mcmc.out, function(x) dim(x$samples)[1]))
+  if(any(iters != iter / thin)){
     N <- min(iters)
-    warning(paste("Variable chain lengths, truncating to minimum=", N))
-  } else {
-    N <- iter/thin
+    warning(paste0("Variable chain lengths, truncating to minimum = ", N))
+  }else{
+    N <- iter / thin
   }
-  samples <- array(NA, dim=c(N, chains, 1+length(par.names)),
-                   dimnames=list(NULL, NULL, c(par.names,'lp__')))
-  if(!skip_unbounded){
+  samples <- array(NA, dim = c(N, chains, 1 + length(par.names)),
+                   dimnames = list(NULL, NULL, c(par.names, "lp__")))
+
+  if(skip_unbounded){
+    samples.unbounded <- NULL
+  }else{
     samples.unbounded <- samples
-  } else {
-    samples.unbounded= NULL
   }
   for(i in 1:chains){
-    samples[,i,] <- mcmc.out[[i]]$samples[1:N,]
+    samples[, i, ] <- mcmc.out[[i]]$samples[1:N, ]
     if(!skip_unbounded)
-      samples.unbounded[,i,] <- cbind(mcmc.out[[i]]$unbounded[1:N,],
-                                      mcmc.out[[i]]$samples[,1+length(par.names)])
+      samples.unbounded[, i, ] <- cbind(mcmc.out[[i]]$unbounded[1:N, ],
+                                        mcmc.out[[i]]$samples[, 1 + length(par.names)])
   }
-  if(algorithm=="NUTS")
-    sampler_params <-
-      lapply(mcmc.out, function(x) x$sampler_params[1:N,])
-  else sampler_params <- NULL
+  if(algorithm == "NUTS"){
+    sampler_params <- lapply(mcmc.out, function(x) x$sampler_params[1:N, ])
+  }else{
+    sampler_params <- NULL
+  }
   time.warmup <- unlist(lapply(mcmc.out, function(x) as.numeric(x$time.warmup)))
   time.total <- unlist(lapply(mcmc.out, function(x) as.numeric(x$time.total)))
   cmd <- unlist(lapply(mcmc.out, function(x) x$cmd))
-  if(N < warmup) warning("Duration too short to finish warmup period")
-  ## When running multiple chains the psv files will either be overwritten
-  ## or in different folders (if parallel is used). Thus mceval needs to be
-  ## done posthoc by recombining chains AFTER thinning and warmup and
-  ## discarded into a single chain, written to file, then call -mceval.
-  ## Merge all chains together and run mceval
-  message(paste("... Merging post-warmup chains into main folder:", path))
+  if(N < warmup){
+    warning("Duration too short to finish warmup period")
+  }
+  # When running multiple chains the psv files will either be overwritten
+  # or in different folders (if parallel is used). Thus mceval needs to be
+  # done posthoc by recombining chains AFTER thinning and warmup and
+  # discarded into a single chain, written to file, then call -mceval.
+  # Merge all chains together and run mceval
+  message(paste0("... Merging post-warmup chains into main folder: ", path))
   samples2 <- do.call(rbind, lapply(1:chains, function(i)
     samples[-(1:warmup), i, -dim(samples)[3]]))
-  .write_psv(fn=model, samples=samples2, model.path=path)
-  ## These already exclude warmup
+  .write_psv(fn = model, samples = samples2, model.path = path)
+  # These already exclude warmup
   unbounded <- do.call(rbind, lapply(mcmc.out, function(x) x$unbounded))
-  oldwd <- getwd(); on.exit(setwd(oldwd))
+  oldwd <- getwd()
+  on.exit(setwd(oldwd))
   setwd(path)
-  write.table(unbounded, file='unbounded.csv', sep=",", col.names=FALSE, row.names=FALSE)
+  write.table(unbounded, file = "unbounded.csv", sep = ",", col.names = FALSE, row.names = FALSE)
   if(mceval){
     message("... Running -mceval on merged chains")
-    system(paste(model, "-mceval"), ignore.stdout=FALSE)
+    system(paste(model, "-mceval"), ignore.stdout = FALSE)
   }
   covar.est <- cov(unbounded)
-  if(!skip_monitor){
-    if(!requireNamespace("rstan", quietly = TRUE))
-      stop("Package 'rstan' is required to calculate diagnostics.\n Install it and try again, or set skip_monitor=FALSE.")
-    message('Calculating ESS and Rhat (skip_monitor=TRUE will skip)...')
-    mon <- rstan::monitor(samples, warmup, print=FALSE)
-  } else {
+  if(skip_monitor){
     message('Skipping ESS and Rhat statistics..')
     mon <- NULL
+  }else{
+    if(!requireNamespace("rstan", quietly = TRUE)){
+      stop("Package 'rstan' is required to calculate diagnostics.\n",
+           "Install it and try again, or set skip_monitor = FALSE",
+           call. = FALSE)
+    }
+    message("Calculating ESS and Rhat (skip_monitor = TRUE will skip)...")
+    mon <- rstan::monitor(samples, warmup, print = FALSE)
   }
-  result <- list(samples=samples, sampler_params=sampler_params,
-                 samples_unbounded=samples.unbounded,
-                 time.warmup=time.warmup, time.total=time.total,
-                 algorithm=algorithm, warmup=warmup,
-                 model=model, max_treedepth=mcmc.out[[1]]$max_treedepth,
-                 cmd=cmd, init=init, covar.est=covar.est, mle=mle,
-                 monitor=mon)
-  result <- adfit(result)
-  return(invisible(result))
+  result <- list(samples = samples,
+                 sampler_params = sampler_params,
+                 samples_unbounded = samples.unbounded,
+                 time.warmup = time.warmup,
+                 time.total = time.total,
+                 algorithm = algorithm,
+                 warmup = warmup,
+                 model = model,
+                 max_treedepth = mcmc.out[[1]]$max_treedepth,
+                 cmd = cmd,
+                 init = init,
+                 covar.est = covar.est,
+                 mle = mle,
+                 monitor = mon)
+
+  invisible(adfit(result))
 }
 
